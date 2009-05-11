6c61f7a5880b94a7ca06e13bf654c895bd1327b3
Ken Igarashi <txe.i.men@gmail.com>
2009-05-12 02:58:35 +0900

    class create

---
+++ sdl/color.h
@@ -0,0 +1,23 @@
+//#include <konoha.h>
+
+//#ifdef
+//#include <SDL/SDL.h>
+#include <konoha.h>
+
+typedef struct knh_sdl_rect_t {
+  knh_int_t x;
+  knh_int_t y;
+  knh_int_t w;
+  knh_int_t h;
+} knh_sdl_rect_t;
+
+
+
+
+typedef struct knh_sdl_color_t {
+  knh_int_t r;
+  knh_int_t g;
+  knh_int_t b;
+} knh_sdl_color_t;
+
+
