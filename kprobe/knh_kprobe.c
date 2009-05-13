c12e8d497592dad739e9d1cbcaf3783392da0776
ken igarasi <ken.igarashi60@gmail.com>
2009-05-14 05:40:50 +0900

    prot

--- kprobe/knh_kprobe.c
+++ kprobe/knh_kprobe.c
@@ -7,4 +7,33 @@
 #include <linux/kprobes.h>
 
 
+METHOD Kprobe_init(Ctx* ctx, knh_sfp_t* sfp)
+{
+  int ret;
+  char* symname = p_char(sfp[1]);
+  struct kprobe kp = {
+    .symbol_name = symname,
+  };
+  kp.pre_handler = handler_pre;
+  kp.post_handler = handler_post;
+  kp.fault_handler = handler_fault;
+  ret = register_kprobe(&kp);
+  if(ret < 0) {
+    printk(KERN_INFO "register_kprobe failed, returned %d\n", ret);
+    return ret;
+  }
+  printk(KERN_INFO "Planted kprobe at %p\n", kp.addr);
+  
+  //return
+}
 
+METHOD Kprobe_exit(Ctx* ctx, knh_sfp_t* sfp)
+{
+  struct kprobe kp = (kprobe)((sfp[1].glue)->ptr);
+  unregister_kprobe(&kp);
+  printk(KERN_INFO "kprobe at %p unregistered\n", kp.addr);
+  KNH_RETURN_void(ctx, sfp);
+}
+
+  
+  
